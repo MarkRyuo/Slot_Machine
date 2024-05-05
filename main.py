db897629618441e1d0100fe953cd733c889bb632
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-05 20:22:01 +0800

    up;date herE

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 def deposit() :
     while True :
         amount = input("What would you like deposit? $ ")
-        if amount.isdigit() :
+        if amount.isdigit() : # isdigit is checking 
             amount = int(amount)
             if amount > 0 :
-                break
+                break # Kapag nilagay ay greater than 0 titigil na ang loop
             else :
                 print("Amount must be greater than 0.")
         else:
