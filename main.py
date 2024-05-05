84a2f43a87db0de60d320005d57f4456214bf8ee
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-05 20:31:32 +0800

    updat herE

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def deposit() :
         amount = input("What would you like deposit? $ ")
         if amount.isdigit() : #  It returns True if all characters in the string are digits (0-9) otherwise false 
             amount = int(amount)
-            if amount > 0 :
+            if amount > 5 :
                 break # Kapag nilagay ay greater than 0 titigil na ang loop
             else :
                 print("Amount must be greater than 0.")
