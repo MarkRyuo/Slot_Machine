4004ef3fd8d472271797663783902892a7889408
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-06 20:04:46 +0800

    update herE

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ def deposit() :
     return amount
 
 def get_number_of_lines() :
+    while True :
+        amount = input("What would you like deposit? $ ")
+        if amount.isdigit() : #  It returns True if all characters in the string are digits (0-9) otherwise false 
+            amount = int(amount)
+            if amount > 5 :
+                break # Kapag nilagay ay greater than 0 titigil na ang loop
+            else :
+                print("Amount must be greater than 5.")
+        else:
+            print("Please enter a number")
+    return amount
 
 
 def main() :
