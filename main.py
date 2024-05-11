1fb7a18d7f2771221e4fda8c0d118cba5a9b36f1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-11 17:52:44 +0800

    update herE

--- main.py
+++ main.py
@@ -35,8 +35,11 @@ def get_slot_machine_spin(rows, cols, symbols) :
 
 def print_slot_machine(columns) :
     for row in range(len(columns[0])) :
-        for column in columns:
-            print(column[row], "|")  
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1 :
+                print(column[row], "|") 
+            else:
+                print(column[row]) 
 
 
 
