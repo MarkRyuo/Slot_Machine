ba9e2355324fe1e359b9dcddfee40f2521a1b825
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-12 16:40:50 +0800

    update herE

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def print_slot_machine(columns) :
     for row in range(len(columns[0])) :
         for i, column in enumerate(columns):
             if i != len(columns) - 1 :
-                print(column[row], "|") 
+                print(column[row], end =" | ") 
             else:
                 print(column[row])
 
